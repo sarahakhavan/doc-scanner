5b1238341ec3e80a7f453b257a9c1dec327b08b2
Sarah <sarah.9473@gmail.com>
2025-05-05 15:25:39 -0400

    Now can find edges in a given picture

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("D1.jpg")
-cv2.imshow("Test", img)
+image = cv2.imread('document.jpg')
+orig = image.copy()
+image = cv2.resize(image, (800, int(image.shape[0] * 800 / image.shape[1])))
+
+
+
+
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(blurred, 50, 150)
+
+cv2.imshow("Edged", edged)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
